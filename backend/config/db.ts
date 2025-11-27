acca2c35c13533d793532251c580994696b51029
Divyesh Mangla <174847645+DivyeshMangla@users.noreply.github.com>
2025-11-27 23:57:03 +0530

    feat: implement in-memory MongoDB connection for development environment

--- backend/config/db.ts
+++ backend/config/db.ts
@@ -1,15 +1,22 @@
 import mongoose from "mongoose";
+import { MongoMemoryServer } from "mongodb-memory-server";
 
 const connectDB = async () => {
-
     try {
-        await mongoose.connect(process.env.MONGO_URI as string);
-        console.log("MongoDB connected successfully");
+        // In-Memory Database - Dev
+        const mongoServer = await MongoMemoryServer.create();
+        const devUri = mongoServer.getUri();
+
+        await mongoose.connect(devUri);
+        console.log("Connected to In-Memory MongoDB (DEV MODE)");
+
+        // Actual MongoDB - Prod
+        // await mongoose.connect(process.env.MONGO_URI as string);
+        // console.log("Connected to MongoDB Atlas (PRODUCTION MODE)");
     } catch (error) {
-        console.error(error);
+        console.error("Database connection error:", error);
         process.exit(1);
     }
-
-}
+};
 
 export default connectDB;
